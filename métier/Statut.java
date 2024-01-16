629bc6718ec6721d56735153a433722bcc8722fe
Julia123456789037 <julia037.filia@gmail.com>
2024-01-16 11:11:24 +0100

    statut.java

--- métier/Statut.java
+++ métier/Statut.java
@@ -1,5 +1,19 @@
 package projet_java.Bibli.java_biblio_LivreV2.métier;
 
-public class Statut {
-    
+public class Statut 
+{
+    private String nom;
+
+
+    public Statut(String nom) 
+    {
+        this.nom = nom;
+    }
+
+    public String getNom() { return nom;}
+    public void setNom(String nom) {this.nom = nom;}
+
+    @Override
+    public String toString() {return "Statut [nom=" + nom + "]";}
+  
 }
